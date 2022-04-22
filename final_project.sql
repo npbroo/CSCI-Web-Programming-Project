d539d5bcfeafa67b3fea569a876bf1ba0ff1207a
Paxton G Chai <pgc54472@uga.edu>
2022-04-22 17:31:48 -0400

    Updated SQL
    added Products SQL

--- final_project.sql
+++ final_project.sql
@@ -1,15 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 10, 2021 at 05:01 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2022 at 11:29 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `final_project`
@@ -21,14 +27,66 @@ SET time_zone = "+00:00";
 -- Table structure for table `person`
 --
 
-CREATE TABLE person (
-  user      VARCHAR(255)   NOT NULL,
-  pass      VARCHAR(255)   NOT NULL,
-  PRIMARY KEY (user)
-);
+CREATE TABLE `person` (
+  `user` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `person`
 --
 
-INSERT INTO `person` (`user`, `pass`) VALUES ('admin', 'password');
+INSERT INTO `person` (`user`, `pass`) VALUES
+('admin', 'teet');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `pid` int(50) NOT NULL,
+  `title` varchar(126) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `price` int(50) NOT NULL,
+  `description` varchar(126) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`pid`, `title`, `image`, `price`, `description`) VALUES
+(1, 'Shirt', 'https://editorial01.shutterstock.com/wm-preview-1500/12907067b/aed9a63b/Shutterstock_12907067b.jpg', 6, 'Shirt');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`user`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
