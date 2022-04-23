0de35a489f2c3a4b0f1e79b6f06695ceb38e2d38
Yamin Himani <yaminhimani@outlook.com>
2022-04-23 10:35:04 -0400

    homepage work

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div className='absolute inset-x-0 bottom-0 mt-auto h-10 flex arch-black-bg items-center text-white'>
+        <div className='fixed inset-x-0 bottom-0 mt-auto h-10 flex arch-black-bg items-center text-white'>
             <span className="m-auto">
                 Footer
             </span>
--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,63 @@ export default function Home() {
           <a href='shop'>
         <img className="mainPage "border="0" alt="Shop Now!" src="https://shop.georgiadogs.com/content/ws/all/725738e9-f168-4afb-be53-1ae1c4810a32.jpg"/>
         </a>
-        <h3 className='featured'>Featured Products</h3>
+        <br></br>
+        <br></br>
+        <h2 className='featured'>Our Featured Products</h2>
+<div className="allFeatured">
+  <div className='featuredprodOne'>
+  <br></br>
+  <br></br>
+  <br></br>
+  <a href='shop'>
+  <img className="tee" src="https://cdn.shopify.com/s/files/1/0112/9739/0655/products/UGAGeorgiaBulldogsChampionT-shirt-Black_480x.jpg?v=1584828835" alt="UGA Black Tee"/>
+  <h1>UGA Cotton Tee</h1>
+  <p className="price">$14.99</p>
+  <button className='addCart'>View Product</button>
+</a>
+ </div>
+
+ <div className='featuredprodTwo'>
+
+
+  <img className="tee" src="https://s.yimg.com/aah/yhst-59135275316139/uga-champion-packable-jacket-white-1.gif" alt="UGA Rain Jacket ="/>
+
+  <a href='shop'>
+
+  <h1>UGA Rain Jacket</h1>
+  <p className="price">$54.99</p>
+  <button className='addCart'>View Product</button>
+</a>
+ </div>
+
+ <div className='featuredprodThree'>
+ <a href='shop'>
+  <img className="tee" src="https://cdn.shopify.com/s/files/1/0112/9739/0655/products/apc03488385_c82105_9901_ftq_mq_1_3216520_1024x1024.jpg?v=1618962934" alt="UGA Shorts ="/>
+  <h1>UGA Champion Mesh Shorts</h1>
+  <p className="price">$29.99</p>
+  <button className='addCart'>View Product</button>
+</a>
+ </div>
+
+ <div className='featuredprodFour'>
+ <a href='shop'>
+
+  <img className="tee" src="https://images.footballfanatics.com/FFImage/thumb.aspx?i=/productimages/_3483000/altimages/ff_3483073-cb395b47a211140d586ealt1_full.jpg&w=900" alt="UGA Rain Jacket ="/>
+  <h1>UGA Hat</h1>
+  <p className="price">$25.99</p>
+  <button className='addCart'>View Product</button>
+</a>
+ </div>
+ <a href='shop'>
+        <img className="shop "border="0" alt="Shop Now!" src="https://shop.georgiadogs.com/content/ws/all/4fcd3a59-94bf-494f-a5bb-32270cde4118__1600X384.jpg"/>
+        </a>
+
+</div>
         </span>
-      </div>
+
+
+        
+   </div>
 
       <Footer />
     </>
