f022fb13d3bdd66b18921fa613057e7f11e8bf0a
Paxton G Chai <pgc54472@uga.edu>
2022-04-25 11:34:56 -0400

    Updated the sql file

--- final_project.sql
+++ final_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2022 at 11:29 PM
+-- Generation Time: Apr 25, 2022 at 05:27 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.4
 
@@ -24,20 +24,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `person`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `person` (
-  `user` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL
+CREATE TABLE `orders` (
+  `oid` int(50) NOT NULL,
+  `quantity` int(50) NOT NULL,
+  `productid` int(50) NOT NULL,
+  `userid` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `person`
---
-
-INSERT INTO `person` (`user`, `pass`) VALUES
-('admin', 'test');
 
 -- --------------------------------------------------------
 
@@ -59,67 +54,46 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(1, 'Shirt 1', 'product_images/ugashirt1.jpg', 6, 'shirt', 'The comfiest UGA shirt you can buy');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(2, 'Shirt 2', 'product_images/ugarainjacket.jpg', 25, 'shirt', 'White UGA Logo rain jacket');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(3, 'Shirt 3', 'product_images/ugatshirt.jpg', 9, 'shirt', 'black UGA athletic T-shirt');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(4, 'Hat 1', 'product_images/ugahat1.jpg', 8, 'hat', 'Red UGA Nike hat');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(5, 'Hat 2', 'product_images/ugahat2.jpg', 10, 'hat', 'Vintage stylized UGA hat red');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(6, 'Hat 3', 'product_images/ugahat3.jpg', 10, 'hat', 'Vintage stylized UGA hat gray');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(7, 'Hat 4', 'product_images/ugahat4.jpg', 8, 'hat', 'Red UGA baseball hat');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(8, 'Hat 5', 'product_images/ugacap.jpg', 9, 'hat', 'All black cap with an embroidered UGA Logo');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(9, 'Jersey 1', 'product_images/ugajersey1.jpg', 30, 'jersey', 'Red and black UGA jersey');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(10, 'Jersey 2', 'product_images/ugajersey2.jpg', 30, 'jersey', 'White and black UGA jersey');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(11, 'Jersey 3', 'product_images/ugajersey3.jpg', 25, 'jersey', 'Red Classic UGA jersey');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(12, 'Jersey 4', 'product_images/ugajersey4.jpg', 35, 'jersey', 'Black and red player Jersey');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(13, 'Pants 1', 'product_images/ugapants1.jpg', 10, 'pants', 'Red UGA athletic shorts');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(14, 'Pants 2', 'product_images/ugapants2.jpg', 10, 'pants', 'Black UGA athletic shorts');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(15, 'Pants 3', 'product_images/ugapants3.jpg', 15, 'pants', 'Black UGA Sweatpants');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
-(16, 'Pants 4', 'product_images/ugapants4.jpg', 15, 'pants', 'Gray UGA Sweatpants');
-
-INSERT INTO `products` (`pid`, `title`, `image`, `price`, `category`, `description`) VALUES
+(1, 'Shirt 1', 'product_images/ugashirt1.jpg', 6, 'shirt', 'The comfiest UGA shirt you can buy'),
+(2, 'Shirt 2', 'product_images/ugarainjacket.jpg', 25, 'shirt', 'White UGA Logo rain jacket'),
+(3, 'Shirt 3', 'product_images/ugatshirt.jpg', 9, 'shirt', 'black UGA athletic T-shirt'),
+(4, 'Hat 1', 'product_images/ugahat1.jpg', 8, 'hat', 'Red UGA Nike hat'),
+(5, 'Hat 2', 'product_images/ugahat2.jpg', 10, 'hat', 'Vintage stylized UGA hat red'),
+(6, 'Hat 3', 'product_images/ugahat3.jpg', 10, 'hat', 'Vintage stylized UGA hat gray'),
+(7, 'Hat 4', 'product_images/ugahat4.jpg', 8, 'hat', 'Red UGA baseball hat'),
+(8, 'Hat 5', 'product_images/ugacap.jpg', 9, 'hat', 'All black cap with an embroidered UGA Logo'),
+(9, 'Jersey 1', 'product_images/ugajersey1.jpg', 30, 'jersey', 'Red and black UGA jersey'),
+(10, 'Jersey 2', 'product_images/ugajersey2.jpg', 30, 'jersey', 'White and black UGA jersey'),
+(11, 'Jersey 3', 'product_images/ugajersey3.jpg', 25, 'jersey', 'Red Classic UGA jersey'),
+(12, 'Jersey 4', 'product_images/ugajersey4.jpg', 35, 'jersey', 'Black and red player Jersey'),
+(13, 'Pants 1', 'product_images/ugapants1.jpg', 10, 'pants', 'Red UGA athletic shorts'),
+(14, 'Pants 2', 'product_images/ugapants2.jpg', 10, 'pants', 'Black UGA athletic shorts'),
+(15, 'Pants 3', 'product_images/ugapants3.jpg', 15, 'pants', 'Black UGA Sweatpants'),
+(16, 'Pants 4', 'product_images/ugapants4.jpg', 15, 'pants', 'Gray UGA Sweatpants'),
 (17, 'Pants 5', 'product_images/ugashorts.jpg', 12, 'pants', 'Dark gray UGA basketball shorts');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(50) NOT NULL,
+  `user` varchar(126) NOT NULL,
+  `pass` varchar(126) NOT NULL,
+  `orderid` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `person`
+-- Indexes for table `orders`
 --
-ALTER TABLE `person`
-  ADD PRIMARY KEY (`user`);
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`oid`);
 
 --
 -- Indexes for table `products`
@@ -128,14 +102,32 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`pid`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `oid` int(50) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(50) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
